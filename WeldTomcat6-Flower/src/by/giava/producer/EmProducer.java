05cae66d78256957c7bd9315980176af67ca967d
hernand.azevedo@gmail.com <hernand.azevedo@gmail.com@212d080f-729e-9039-a2dd-4b18e2f23d7e>
2013-07-13 02:53:43 +0000

    Correção do producer. Ele deve ser Application, e retornar um entityMAnager por request. Alem disso, no invalidate da sessao, o injection point do entityManager precisa ter contexto. Por isso, ApplicationScoped.

--- WeldTomcat6-Flower/src/by/giava/producer/EmProducer.java
+++ WeldTomcat6-Flower/src/by/giava/producer/EmProducer.java
@@ -2,13 +2,17 @@ package by.giava.producer;
 
 import java.io.Serializable;
 
-import javax.enterprise.context.ConversationScoped;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 
 import org.jboss.seam.solder.core.ExtensionManaged;
 
+@Named
+@ApplicationScoped
 public class EmProducer implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -16,11 +20,8 @@ public class EmProducer implements Serializable {
 	@ExtensionManaged
 	@Produces
 	@PersistenceUnit(unitName = "pu")
-	@ConversationScoped
+	@RequestScoped
 	EntityManagerFactory em;
 
-	public EmProducer() {
-		System.out.println("start em producer");
-	}
 
 }
