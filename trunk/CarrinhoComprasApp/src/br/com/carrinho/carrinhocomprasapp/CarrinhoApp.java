27fefae004a6240c7f7e2c1f741ab6d0bc76a761
hernand.azevedo@gmail.com <hernand.azevedo@gmail.com@212d080f-729e-9039-a2dd-4b18e2f23d7e>
2013-12-02 15:53:51 +0000

    Activity de Configurações

--- trunk/CarrinhoComprasApp/src/br/com/carrinho/carrinhocomprasapp/CarrinhoApp.java
+++ trunk/CarrinhoComprasApp/src/br/com/carrinho/carrinhocomprasapp/CarrinhoApp.java
@@ -1,10 +1,14 @@
 package br.com.carrinho.carrinhocomprasapp;
 
+import android.app.AlarmManager;
 import android.app.Application;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.NetworkInfo.State;
+import android.preference.PreferenceManager;
 import android.telephony.TelephonyManager;
 
 public class CarrinhoApp extends Application {
@@ -30,6 +34,86 @@ public class CarrinhoApp extends Application {
 		// not guaranteed to be called
 		super.onTerminate();
 	}
+	
+	// Schedule AlarmManager to invoke DealAlarmReceiver and cancel any existing
+		// current PendingIntent
+		// we do this because we *also* invoke the receiver from a BOOT_COMPLETED
+		// receiver
+		// so that we make sure the service runs either when app is
+		// installed/started, or when device boots
+		public void scheduleAlarmReceiver() {
+			
+			
+			scheduleAlarmReceiver(this);
+		}
+		
+		// Schedule AlarmManager to invoke DealAlarmReceiver and cancel any existing
+				// current PendingIntent
+				// we do this because we *also* invoke the receiver from a BOOT_COMPLETED
+				// receiver
+				// so that we make sure the service runs either when app is
+				// installed/started, or when device boots
+				public static void scheduleAlarmReceiver(Context ctx) {
+					
+					
+					AlarmManager alarmMgr = (AlarmManager) ctx
+							.getSystemService(Context.ALARM_SERVICE);
+					PendingIntent pendingIntent = PendingIntent.getBroadcast(ctx, 0,
+							new Intent(ctx, CarrinhoAlarmReceiver.class),
+							PendingIntent.FLAG_CANCEL_CURRENT);
+
+					
+					String freq = PreferenceManager.getDefaultSharedPreferences(ctx).getString("sync_frequency", null);
+					
+					if(freq == null || freq.equals("-1")) return;
+				   
+				   long intervalo = Constants.ALARM_INTERVAL;
+				   if(freq.equals("10"))
+					   intervalo =  Constants.ALARM_INTERVAL_TEN_SECODS;
+				   else if(freq.equals("60"))
+					   intervalo =  Constants.ALARM_INTERVAL_HOUR;
+					
+					
+					// Use inexact repeating which is easier on battery (system can phase
+					// events and not wake at exact times)
+					alarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+							Constants.ALARM_TRIGGER_AT_TIME, intervalo,
+							pendingIntent);
+				}
+		
+				
+				// Schedule AlarmManager to invoke DealAlarmReceiver and cancel any existing
+				// current PendingIntent
+				// we do this because we *also* invoke the receiver from a BOOT_COMPLETED
+				// receiver
+				// so that we make sure the service runs either when app is
+				// installed/started, or when device boots
+				public static void scheduleAlarmReceiver(Context ctx,String freq) {
+					
+					
+					AlarmManager alarmMgr = (AlarmManager) ctx
+							.getSystemService(Context.ALARM_SERVICE);
+					PendingIntent pendingIntent = PendingIntent.getBroadcast(ctx, 0,
+							new Intent(ctx, CarrinhoAlarmReceiver.class),
+							PendingIntent.FLAG_CANCEL_CURRENT);
+
+					
+					
+					if(freq == null || freq.equals("-1")) return;
+				   
+				   long intervalo = Constants.ALARM_INTERVAL;
+				   if(freq.equals("10"))
+					   intervalo =  Constants.ALARM_INTERVAL_TEN_SECODS;
+				   else if(freq.equals("60"))
+					   intervalo =  Constants.ALARM_INTERVAL_HOUR;
+					
+					
+					// Use inexact repeating which is easier on battery (system can phase
+					// events and not wake at exact times)
+					alarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+							Constants.ALARM_TRIGGER_AT_TIME, intervalo,
+							pendingIntent);
+				}
 
 	public boolean connectionPresent() {
 		NetworkInfo netInfo = cMgr.getActiveNetworkInfo();
--- trunk/CarrinhoComprasApp/src/br/com/carrinho/carrinhocomprasapp/CarrinhoBootReceiver.java
+++ trunk/CarrinhoComprasApp/src/br/com/carrinho/carrinhocomprasapp/CarrinhoBootReceiver.java
@@ -5,12 +5,25 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class CarrinhoBootReceiver extends BroadcastReceiver {
 
 	   @Override
 	   public void onReceive(Context context, Intent intent) {
+		   
+		   String freq = PreferenceManager.getDefaultSharedPreferences(context).getString("sync_frequency", null);
+			
+			if(freq == null || freq.equals("-1")) return;
+		   
+		   long intervalo = Constants.ALARM_INTERVAL;
+		   if(freq.equals("10"))
+			   intervalo =  Constants.ALARM_INTERVAL_TEN_SECODS;
+		   else if(freq.equals("60"))
+			   intervalo =  Constants.ALARM_INTERVAL_HOUR;
+		   
+		   
 	      Log.i(Constants.LOG_TAG, "DealBootReceiver invoked, configuring AlarmManager");
 	      AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 	      PendingIntent pendingIntent =
@@ -18,6 +31,6 @@ public class CarrinhoBootReceiver extends BroadcastReceiver {
 
 	      // use inexact repeating which is easier on battery (system can phase events and not wake at exact times)
 	      alarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, Constants.ALARM_TRIGGER_AT_TIME,
-	               Constants.ALARM_INTERVAL, pendingIntent);
+	    		  intervalo, pendingIntent);
 	   }
 }
